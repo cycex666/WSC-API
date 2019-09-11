f638dd9f5e76b514da03d3d1d172fda492a0e03b
Czuchraj <krzysztof.czuchraj@leaware.com>
2019-09-11 14:00:27 +0200

    add mongodb configuration

---
+++ src/modules/cars/controllers/update.car.controller.ts
@@ -0,0 +1,32 @@
+import {Request, Response, NextFunction} from 'express';
+import ow from 'ow';
+
+import {HTTP400Error} from '../../../core/errors';
+
+import * as model from '../models';
+
+export const exec = async (req: Request, res: Response): Promise<void> => {
+	const {params, body} = req;
+	const {organisation, id} = params;
+
+	res.status(200).json(await model.Update(organisation, id, body));
+};
+
+export const validate = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	const {params, body} = req;
+	const {id, organisation} = params;
+
+	try {
+		ow(organisation, 'organisation', ow.string.nonEmpty);
+		ow(id, 'id', ow.string.nonEmpty);
+		ow(body, 'body', ow.object.nonEmpty.exactShape({
+			vin: ow.string.nonEmpty,
+			carModel: ow.string.nonEmpty,
+			brand: ow.string.nonEmpty
+		}));
+
+		next();
+	} catch (err) {
+		throw new HTTP400Error(err.message);
+	}
+};
